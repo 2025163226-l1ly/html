37dd87b73e1b6ede66988c18b35dee5a52a0dcbe
2025163226-l1ly <2025163226@ub.edu.bz>
2025-10-02 15:49:42 -0600

    Add files via upload

---
+++ Course Page.cpp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1><b>Course Page</h1></b>
+<p>This course is an introduction to the fundamentals of Computer Science that will provide students with a foundation for future courses in Computer Science.
+ The course is a broad survey of the discipline through topics that allow students to understand the capabilities of computer systems in terms of: the computer architecture;
+ operating systems and computer networks; software and software development; data organization and visualization; and the potential of computing machines.</p>
+</body>
+</html>
